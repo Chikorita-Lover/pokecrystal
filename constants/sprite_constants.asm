da6fc77c180bb4ee55a3e395556e895a158dc31b
Chikorita Lover <cjgardels@gmail.com>
2023-08-29 21:49:09 -0500

    Update running sprites
    Update Chris running sprite
    Add unique Kris running sprite

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -107,6 +107,7 @@
 	const SPRITE_CHRIS_SURF ; 67
 	const SPRITE_KRIS_SURF ; 68
 	const SPRITE_CHRIS_RUN ; 69
+	const SPRITE_KRIS_RUN ; 6a
 DEF NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; SpriteMons indexes (see data/sprites/sprite_mons.asm)
